b9ee3977ef5a6eb1574e310ef78384c725375170
emoen <endremoen@hotmail.com>
2013-05-08 11:21:14 +0200

    3rd commit

--- src/miniature/HelloWorld2.scala
+++ src/miniature/HelloWorld2.scala
@@ -4,11 +4,18 @@ object HelloWorld2 {
   def main(args:Array[String]):Unit = {
     println(args(0))
     var greeting = ""
-    for (i <- 0 until args.length) {
+    val range = 0.until(args.length) //val -> var with final
+    for (i <- range) {
+    //for (i <- 0 until args.length) {
       greeting += (args(i) + " ")
+    }
+    var greeting1:String = ""
+    args.foreach { arg =>
+    	greeting1 += (arg + " ")
     }
     if (args.length > 0) greeting = greeting.substring(0, greeting.length - 1)
  
     println(greeting)
+    println(greeting1)
   }
 }
