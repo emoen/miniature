5f45a9222ce50a6edc1c4ba580a551aa74a8a876
emoen <endremoen@hotmail.com>
2016-01-20 09:24:49 +0100

    playing with selfinvoking functions

--- javascript/selfInvokingFunction.js
+++ javascript/selfInvokingFunction.js
@@ -1,4 +1,29 @@
+var invokeFunction = (function (){
+	console.log("hello invoke");
+});
+
+invokeFunction();
+
+var selfInvoke = ( function() {
+	var dummy = "hello selfinvoke closure";
+	return function() { console.log(dummy);}
+})();
+
+selfInvoke();
+
+var selfSelfInvoke = ( function() {
+	var closure1 = "hello selfinvoke closure1";
+	return function() {
+		var closure2 = "hello selfinvoke closure2";
+		return function() { console.log("closure1:"+closure1+" closure2:"+closure2);}
+	}();
+})();
+
+selfSelfInvoke();
+
 var add = (function () {
+	//javascript closure
+	// A closure is a function having access to the parent scope, even after the parent function has closed. 
     var counter = 0;
     return function () {return counter += 1;}
 })();
@@ -6,4 +31,4 @@ var add = (function () {
 console.log(add());
 console.log(add());
 console.log(add());
-
+console.log(add);
