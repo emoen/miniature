73d93bc9d42645d3194573ec4ce7105440dff13a
emoen <endremoen@hotmail.com>
2016-01-22 12:39:46 +0100

    added more map functions

--- javascript/map.js
+++ javascript/map.js
@@ -4,11 +4,21 @@ var $ = require('jquery')(jsdom.jsdom().defaultView);
 $("<h1>test passes</h1>").appendTo("body");
 console.log($("body").html());
 
-legendLayers = [{'id':123, 'legend':'abc'},{'id':324, 'legend':'asdf'}]
+var legendLayers = [{'id':123, 'legend':'abc'},{'id':324, 'legend':'asdf'}]
 console.log("ids:"+Object.keys(legendLayers));
 
-keys = $.map(legendLayers, function(v, i){
+var keys = $.map(legendLayers, function(v, i){
 	console.log("v:"+v.id+" i:"+i);
-  return v.id;
+	return v.id;
 });
 console.log(keys);
+
+var anId = 123;
+var object123 = $.map(legendLayers, function(v, i){
+	console.log("anId:"+anId);
+	if ( v.id == anId) {
+		return v.legend;
+	}
+});
+console.log("obj:"+object123);
+
