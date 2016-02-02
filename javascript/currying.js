17d2c07786f84579ed1235efcf1bb6506444fb15
emoen <endremoen@hotmail.com>
2016-02-02 08:43:32 +0100

    curryed it

--- javascript/currying.js
+++ javascript/currying.js
@@ -4,12 +4,12 @@
 
 function curry2(f) {
 	var that = this;
-	console.log("function:"+f);
+//	console.log("function:"+f);
 	return function(a) {
-		console.log("a:"+a);
+//		console.log("a:"+a);
 		return function(b) {
-			console.log("b:"+b);
-			console.log("that:"+that);
+//			console.log("b:"+b);
+//			console.log("that:"+that);
 			return f.call(that,a,b);
 		}
 	}
@@ -24,10 +24,35 @@ having mapped every element through a function.
 Our choice of 'f' is basically Array.
 Well ignore anything exotic, and just pretend length is good enough to scan the elements. 
 */
-var add4 = function(a ){
-	return function(b) { 
-		return 4+b;
-	}();
+var add = function(a, b ){
+	return a + b;
 };
 
-console.log(add4(5));
+console.log("curry2:"+curry2(add));
+var curry1 = curry2(add)(4);
+console.log("curry1:"+curry1);
+console.log("curryed:"+curry1(5));           // curryed 9
+console.log("curryed:"+(curry2(add)(2))(3)); // curryed 5
+
+var greetCurried = function(greeting) {
+	return function(name) {
+		console.log(greeting + ", " + name);
+	};
+};
+
+var greetHello = greetCurried("Hello");
+greetHello("Heidi"); //"Hello, Heidi"
+greetHello("Eddie"); //"Hello, Eddie"
+greetCurried("Hi there")("Howard"); //"Hi there, Howard"
+
+var greetDeeplyCurried = function(greeting) {
+	return function(separator) {
+		return function(emphasis) {
+			return function(name) {
+				console.log(greeting + separator + name + emphasis);
+			};
+		};
+	};
+};
+var greetAwkwardly = greetDeeplyCurried("Hello")("...")("?");
+greetAwkwardly("Heidi"); //"Hello...Heidi?"
