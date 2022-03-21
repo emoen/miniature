438c17c821a1188ca6524e4ef5f57b72f95c6f4e
Endre Moen <emoen@users.noreply.github.com>
2022-03-21 16:07:02 +0100

    Update trend.py

--- trend.py
+++ trend.py
@@ -1,6 +1,26 @@
 import pytrends
 from pytrends.request import TrendReq
 pytrends = TrendReq(hl="en-US", tz=360)
-pytrends.build_payload(kw_list=["NFT's"])
+kw_list = ["NFT's"]
+kw_list = ["oil"]
+kw_list = ["ukraine"]
+pytrends.build_payload(kw_list=kw_list)
+#pytrend.build_payload(kw_list, timeframe='today 1-m')
+
+historicaldf = pytrend.get_historical_interest(
+  kw_list, 
+  year_start=2020, month_start=10, day_start=1, hour_start=0, 
+  year_end=2022, month_end=2, day_end=21, hour_end=15, cat=0, geo='', gprop='', sleep=0)
+
+historicaldf.plot(figsize=(20, 12))
+historicaldf.plot(subplots=True, figsize=(20, 12))
+
 df = pytrends.interest_over_time()
-df["NFT's"].plot(figsize=(20,7), color='purple', linewidth=7)
+
+adf = df[df.index > '2020-01-01']
+
+df.plot(figsize=(20,7), color='purple', linewidth=7, label=kw_list)
+adf["NFT's"].plot(figsize=(20,7), color='purple', linewidth=7)
+adf["oil"].plot(figsize=(20,7), color='purple', linewidth=7)
+adf["ukraine"].plot(figsize=(20,7), color='purple', linewidth=7)
+
