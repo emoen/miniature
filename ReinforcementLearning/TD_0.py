6d8da3c74eff04f2180734a614656680cc8d3370
Endre Moen <emoen@users.noreply.github.com>
2022-08-17 21:43:26 +0200

    sarsa and q-learning

--- ReinforcementLearning/TD_0.py
+++ ReinforcementLearning/TD_0.py
@@ -54,9 +54,6 @@ V = {}
 for s in range(env.observation_space.n):
     V[s] = 0.0
 
-
-
-
     # num episodes, timesteps
 gamma = 0.90  # discount factor    
 alpha = 0.85  # learning rate
@@ -79,11 +76,11 @@ plt.show()
     
     
 ############### policy #########################
-
+#https://www.geeksforgeeks.org/sarsa-reinforcement-learning/
 
 
 #Function to choose the next action
-def choose_action(state):
+def choose_action(state): #eps-greedy
     action=0
     if np.random.uniform(0, 1) < epsilon: #with probability 1-epsilon do random move
         action = env.action_space.sample()
@@ -92,13 +89,22 @@ def choose_action(state):
     return action
  
 #Function to learn the Q-value
-def update(s, s2, reward, a, a2):
+def update(s, s2, reward, a, a2): #sarsa
     predict = Q[s, a]
     if s==13 and a==2:
         tmp_target = reward + gamma * Q[s2, a2]
         #print(str( Q[s,a] ) + " "+ str( Q[s, a] + alpha * (tmp_target - predict) )+ " next state action:"+str(s2)+" "+str(a2)+ " "+str(Q[14,:]) )
     target = reward + gamma * Q[s2, a2]
     Q[s, a] = Q[s, a] + alpha * (target - predict)
+    
+#Function to learn the Q-value
+def q_update(s, s2, reward, a): #Q-learning
+    predict = Q[s, a]
+    if s==13 and a==2:
+        tmp_target = reward + gamma * np.max( Q[s2, :] )
+        #print(str( Q[s,a] ) + " "+ str( Q[s, a] + alpha * (tmp_target - predict) )+ " next state action:"+str(s2)+" "+str(a2)+ " "+str(Q[14,:]) )
+    target = reward + gamma * np.max( Q[s2, :] )
+    Q[s, a] = Q[s, a] + alpha * (target - predict)    
 
 
 epsilon = 0.1
@@ -108,7 +114,6 @@ Q = np.zeros((env.observation_space.n, env.action_space.n)) # Q matrix
 reward=0
 
 #0: LEFT 1: DOWN 2: RIGHT 3: UP
-#def sarsa(episode, ):
 # Starting the SARSA learning
 VV = np.zeros(num_episodes*num_timesteps)
 VV13 = np.zeros(num_episodes*num_timesteps)
@@ -117,23 +122,26 @@ counter = 0
 for episode in range(num_episodes):
     t = 0
     s1 = env.reset()
-    a1 = choose_action(s1)
+    
+    a1 = choose_action(s1) #sarsa
  
     while t < num_timesteps:
-        #Visualizing the training
-        #env.render()
+        a1 = choose_action(s1) #Q-learning
+        
+        #env.render() #Visualizing the training
          
         #Getting the next state
         s2, reward, done, info = env.step(a1)
  
         #Choosing the next action
-        a2 = choose_action(s2)
+        a2 = choose_action(s2) #sarsa
          
         #Learning the Q-value
-        update(s1, s2, reward, a1, a2)
+        #update(s1, s2, reward, a1, a2) #sarsa
+        q-update(s1, s2, reward, a1)    #Q-learning
  
         s1 = s2
-        a1 = a2
+        a1 = a2 #sarsa
          
         #Updating the respective vaLues
         t += 1
@@ -161,5 +169,37 @@ plt.show()
 
 np.set_printoptions(suppress=True)
 
+# Starting the Q-learning learning
+VV = np.zeros(num_episodes*num_timesteps)
+VV13 = np.zeros(num_episodes*num_timesteps)
+VV10 = np.zeros(num_episodes*num_timesteps)
+counter = 0
+for episode in range(num_episodes):
+    t = 0
+    s1 = env.reset()
+ 
+    while t < num_timesteps:
+        a1 = choose_action(s1) #Q-learning
+        s2, reward, done, info = env.step(a1) #Getting the next state   
+        q_update(s1, s2, reward, a1)    #Q-learning
+ 
+        s1 = s2
+        t += 1         #Updating the respective vaLues
+        reward += 1
+        
+        VV10[counter] = Q[10,1] # 10 step and step down    
+        VV13[counter] = Q[13, 2] # 13 step and step right    
+        VV[counter] = Q[14, 2] # last step and step right        
+        counter +=1
+        
+        if done: #If at the end of learning process
+            break
 
+plt.plot(VV[0:counter])
+plt.show()     
 
+plt.plot(VV13[0:counter])
+plt.show()    
+
+plt.plot(VV10[0:counter])
+plt.show() 
